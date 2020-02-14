7c2e5e1f6eff2273310eef359c342f572372e882
Krystal <Krystal@KKLIM>
2020-02-14 18:13:10 +0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* using (var db = new AdventureWorks2017Entities())
+             {
+                 var firstnameEmployees = from x in db.People
+                                          group x by x.FirstName into numberCount
+                                          orderby numberCount.Key
+                                          select numberCount;
+
+                 if (firstnameEmployees.Count() != 0)
+                 {
+                     foreach (var user in firstnameEmployees)
+                     {
+
+                         Console.WriteLine($"{user.Key},({user.Count()})");
+                     }
+                 }
+                 else
+                 {
+                     Console.WriteLine("There are no users in the db.");
+                 }*/
+
+            /*Console.WriteLine("Please Enter your MiddleName.");
+            var middleNameInput = Console.ReadLine();
+
+            using (var db = new AdventureWorks2017Entities())
+            {
+                var firstnameEmployees = from x in db.People
+                                         select x ;
+
+                if (firstnameEmployees.Count() != 0)
+                {
+                    foreach (var user in firstnameEmployees)
+                    {
+                        *//*var userMiddleName = user.MiddleName;*//*
+
+                        if (user.MiddleName != null)
+                        {
+                            if (user.MiddleName.StartsWith(middleNameInput) == true || user.MiddleName.Contains(middleNameInput) == true)
+                            {
+                                Console.WriteLine($"{user.FirstName},{user.MiddleName}, {user.LastName}");
+                            }
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("There are no users in the db.");
+                }*/
+
+
+
+            Console.WriteLine("Please Enter your Email.");
+            var emailInput = Console.ReadLine();
+
+            using (var db = new AdventureWorks2017Entities())
+            {
+
+
+                var emailEmployees = from x in db.EmailAddresses 
+                                     join y in db.People on
+                                     x.BusinessEntityID equals y.BusinessEntityID
+                                     select new { firstName = y.FirstName, middleName = y.MiddleName, lastName = y.LastName, email = x.EmailAddress1};
+
+
+                if (emailEmployees.Count() != 0)
+                {
+                    foreach (var user in emailEmployees)
+                    {
+                        /*var userMiddleName = user.MiddleName;*/
+
+                        if (user.email != null)
+                        {
+                            if (user.email.Contains(emailInput) == true )
+                            {
+
+                                var json = JsonConvert.SerializeObject(new { firstname = user.firstName, middlename = user.middleName, lastname = user.lastName, email = user.email });
+                                Console.WriteLine(json);
+
+
+
+                                System.IO.File.AppendAllText(@"C:\Users\Krystal\Desktop\linqtext.txt", json, Encoding.UTF8);
+                            }
+                        }
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("There are no users in the db.");
+                }
+
+
+
+
+            }
+        Console.ReadLine();
+        }
+    }
+}
