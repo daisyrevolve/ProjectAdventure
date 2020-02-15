1aa73576f7ac9bacd405b6e443fdbce64b357e17
daisyrevolve <52348791+daisyrevolve@users.noreply.github.com>
2020-02-15 21:18:12 +0800

    final commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjectAdventure
 {
@@ -68,33 +68,30 @@ namespace ProjectAdventure
 
             using (var db = new AdventureWorks2017Entities())
             {
-
-
-                var emailEmployees = from x in db.EmailAddresses 
+                var emailEmployees = from x in db.EmailAddresses
                                      join y in db.People on
                                      x.BusinessEntityID equals y.BusinessEntityID
-                                     select new { firstName = y.FirstName, middleName = y.MiddleName, lastName = y.LastName, email = x.EmailAddress1};
-
+                                     select new { firstName = y.FirstName, middleName = y.MiddleName, lastName = y.LastName, email = x.EmailAddress1 };
 
                 if (emailEmployees.Count() != 0)
                 {
+                    var userList = new List<UserDetails>();
+
                     foreach (var user in emailEmployees)
                     {
-                        /*var userMiddleName = user.MiddleName;*/
 
-                        if (user.email != null)
+                        if (user.email != null && user.email.Contains(emailInput) == true)
                         {
-                            if (user.email.Contains(emailInput) == true )
-                            {
+                            var userNameDetails = user.firstName + " " + user.middleName + " " + user.lastName;
+                            userList.Add(new UserDetails(userNameDetails, user.email));
 
-                                var json = JsonConvert.SerializeObject(new { firstname = user.firstName, middlename = user.middleName, lastname = user.lastName, email = user.email });
-                                Console.WriteLine(json);
-
-
-
-                                System.IO.File.AppendAllText(@"C:\Users\Krystal\Desktop\linqtext.txt", json, Encoding.UTF8);
-                            }
+                            /*var json = JsonConvert.SerializeObject(new { firstname = user.firstName, middlename = user.middleName, lastname = user.lastName, email = user.email });*/
+                            var json = JsonConvert.SerializeObject(userList);
+                            /*var json = JsonSerializer.Serialize(userList);*/
+                            Console.WriteLine(json);
+                            File.AppendAllText(@"C:\Users\Krystal\Desktop\linqtext.txt", json, Encoding.UTF8);
                         }
+
 
                     }
                 }
@@ -102,12 +99,10 @@ namespace ProjectAdventure
                 {
                     Console.WriteLine("There are no users in the db.");
                 }
-
-
-
-
             }
-        Console.ReadLine();
+
+
+
         }
     }
 }
